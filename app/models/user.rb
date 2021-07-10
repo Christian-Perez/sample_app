ccc6ffa007a0f910fe66038e40c573035fe798f3
Christian <Christian@insomniac.digital>
2021-07-10 00:52:48 -0700

    fixes issue with email not being saved to db on user.save! even with success message from console

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-
+  before_save self.email.downcase!
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  before_save { self.email = email.downcase! }
-
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
 
   has_secure_password
