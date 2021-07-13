cab0a10ad25aa9fe3de01d9f118bb81752079a4c
Christian <Christian@insomniac.digital>
2021-07-12 20:12:28 -0700

    updates Usermodel > firxes no_method error that was preventing User.email from persisting to DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   #email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save { self.email = email.downcase! }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  #before_save { self.email = email.downcase! }
+  before_save   self.email.downcase!
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   #password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_nil: true
   has_secure_password
 end
