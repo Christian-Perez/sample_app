3814f062455ecf8136a6385b927a140d3439267b
Christian <Christian@insomniac.digital>
2021-07-17 22:23:46 -0700

    start 9.1.2 - adds remember_digest to db, migrates, refactors User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
-# class User < ApplicationRecord
-#   validates :name, presence: true, length: { maximum: 50 }
-#   before_save self.email.downcase!
-#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-#   validates :password, length: { minimum: 6 }
-#
-#   has_secure_password
-# end
-
 class User < ApplicationRecord
+  attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,10 +10,20 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  #attr_accessible :email, :name, :password, :password_confirmation
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    # returns the hash digest of the given string
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      secureRandom.urlsafe_base64
+    end
+
+    def remember
+      remember_token = new_token
+      update_attribute(:remember_digest, digest(remember_token))
+    end
   end
 end
