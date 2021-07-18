f931fdb4eff92cb25aaed8331d0a715d12d09b72
Christian <Christian@insomniac.digital>
2021-07-18 16:35:46 -0700

    reformats User.rb for readability, sessions persist through browser close |NOTE persistent sessions are not created when a new user is logged in automatically

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,32 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  # Name
+  validates(
+    :name,
+    presence: true,
+    length: { maximum: 50 }
+  )
 
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  # Email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+  before_save { self.email = email.downcase }
+  validates(
+    :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: true
+  )
+
+  # Password
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_nil: true
 
+  # Session
+  attr_accessor :remember_token
+
+  # TODO: talk about this > why digest & new_token can be defined on 'self'
+  #       but remember and authenticated? have to be defined
+  ##      > methods defined with 'class << self' are only visible to the user_super_class?
   class << self
     # returns the hash digest of the given string
     def digest(string)
@@ -18,12 +35,29 @@ class User < ApplicationRecord
     end
 
     def new_token
-      secureRandom.urlsafe_base64
+      SecureRandom.urlsafe_base64
     end
+  end
 
-    def remember
-      remember_token = new_token
-      update_attribute(:remember_digest, digest(remember_token))
-    end
+  # NOTE: remember & authenticated? need to interact with the database; digest and new_token do not
+  # NOTE: remember is called in the sessions_controller
+  def remember
+    # ? the user_in_memory.remember_token is set to the result of model:user.new_token THEN persisted to db
+    self.remember_token = User.new_token
+    # if(
+    update_attribute(:remember_digest, User.digest(remember_token))
+    # ) then puts 'it worked' else puts "it didn't work" end
+    ## it worked
+  end
+
+  # returns true if the given token matches the remember_digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
